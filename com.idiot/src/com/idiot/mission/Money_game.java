0a31551581ab301f883e946ea57d7929c952e63e
JIN <wlsgur7701@naver.com>
2020-06-28 18:20:37 +0900

    200628_1

--- com.idiot/src/com/idiot/mission/Money_game.java
+++ com.idiot/src/com/idiot/mission/Money_game.java
@@ -1,12 +1,12 @@
-package com.practice;
+package com.idiot.mission;
 
 import java.util.Scanner;
 
-public class Practice1 {
+public class Money_game {
+
 	public static void main(String[] args) {
-		
 		Scanner scan = new Scanner(System.in);
-	
+		
 		int[] money = {50000,10000,5000,1000,500,100,50,10,5,1 };
 
 		int[] m_count= new int[10];
@@ -19,7 +19,7 @@ public class Practice1 {
 		
 		
 		
-		
+
 		
 		
 		
@@ -29,20 +29,14 @@ public class Practice1 {
 			m_count[i] = x / money[i];
 			x = x % money[i];
 			System.out.printf("%d 원 : %d 개\n", money[i], m_count[i]);
-		}
+
 	}
+		int sum=0;
+		for(int i =0; i<m_count.length; i++) {
+			sum+=money[i]*m_count[i];
+		}
+System.out.println(sum);
 }
 	
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
+}
